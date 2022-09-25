05e5f3d6a35ec2ab4d86a37d1e3f5aeda30203d3
mousliiim <mouslimby77@gmail.com>
2022-09-25 19:54:52 +0200

    Premier Commit ToUpper et ToLower

---
+++ Makefile
@@ -0,0 +1,28 @@
+SRCS	= ft_bzero.c ft_isalpha.c ft_isdigit.c ft_memchr.c ft_memcpy.c ft_memset.c \
+			ft_atoi.c ft_isalnum.c ft_isascii.c ft_isprint.c ft_memcmp.c ft_memmove.c ft_strlen.c \
+			ft_strdup.c ft_strncmp.c ft_toupper.c ft_tolower.c ft_calloc.c ft_strnstr.c \
+			ft_strchr.c ft_strchr.c ft_strlcpy.c ft_strlcat.c ft_substr.c ft_strjoin.c \
+			ft_strtrim.c
+
+NAME	= libft.a
+
+OBJS	= ${SRCS:.c=.o}
+
+
+HEADERS	= ./
+
+.c.o	:
+		gcc -Wall -Wextra -Werror -I ${HEADERS} -c $< -o ${<:.c=.o}
+
+${NAME} : ${OBJS}
+		ar rcs ${NAME} ${OBJS}
+
+all : ${NAME}
+
+clean :
+		rm -f ${OBJS}
+
+fclean : clean
+		rm -f ${NAME}
+
+re : fclean all
---
+++ libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmourdal <mmourdal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/23 22:03:03 by mouslimmo         #+#    #+#             */
+/*   Updated: 2022/09/25 19:43:49 by mmourdal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+int		ft_tolower(int c);
+int     ft_toupper(int c);
+
+#endif
