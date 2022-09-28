62fce76b917c48462a976a8d34ce3f58136985f3
mousliiim <mouslimby77@gmail.com>
2022-09-28 22:35:16 +0200

    ft_isalnum

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,28 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmourdal <mmourdal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/23 22:03:03 by mouslimmo         #+#    #+#             */
-/*   Updated: 2022/09/28 22:27:19 by mmourdal         ###   ########.fr       */
+/*   Created: 2022/09/28 22:21:20 by mmourdal          #+#    #+#             */
+/*   Updated: 2022/09/28 22:25:49 by mmourdal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
-
-int	ft_tolower(int c);
-int	ft_toupper(int c);
-int	ft_atoi(const char *str);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isdigit(int c);
-int	ft_isprint(int c);
-int	ft_isalnum(int c);
-#endif
+int	ft_isalnum(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
--- ft_isprint.c
+++ ft_isprint.c
@@ -1,28 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmourdal <mmourdal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/23 22:03:03 by mouslimmo         #+#    #+#             */
-/*   Updated: 2022/09/28 22:27:19 by mmourdal         ###   ########.fr       */
+/*   Created: 2022/09/28 22:10:02 by mmourdal          #+#    #+#             */
+/*   Updated: 2022/09/28 22:16:42 by mmourdal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
-
-int	ft_tolower(int c);
-int	ft_toupper(int c);
-int	ft_atoi(const char *str);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isdigit(int c);
-int	ft_isprint(int c);
-int	ft_isalnum(int c);
-#endif
+int	ft_isprint(int c)
+{
+	if (c >= 32 && c <= 126)
+		return (1);
+	return (0);
+}
