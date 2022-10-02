e67cfd8b33e81ba46d02fbc38aa517ce4cfacdb0
mousliiim <mouslimby77@gmail.com>
2022-10-02 18:29:59 +0200

    Maj

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,11 +6,10 @@
 /*   By: mmourdal <mmourdal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/02 17:50:58 by mmourdal          #+#    #+#             */
-/*   Updated: 2022/10/02 18:16:24 by mmourdal         ###   ########.fr       */
+/*   Updated: 2022/10/02 18:25:52 by mmourdal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-#include <string.h>
 
 char	*ft_strdup(const char *s)
 {
@@ -32,16 +31,4 @@ char	*ft_strdup(const char *s)
 	}
 	str[i] = 0;
 	return (str);
-}
-
-int	main(int argc, char **argv)
-{
-	if(argc == 2)
-	{
-		printf("FT_STRDUP : %s\n", ft_strdup(argv[1]));
-		printf("STRDUP : %s\n", strdup(argv[1]));
-	}
-	else
-	printf("Erreur: Il manque un Argument\n");
-	return (0);
 }
