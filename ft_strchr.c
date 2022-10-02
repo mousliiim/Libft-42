295fb9c834befee592ff9330aec47ea2fedc27a6
mousliiim <mouslimby77@gmail.com>
2022-10-02 17:49:16 +0200

    strlen & strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,30 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmourdal <mmourdal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/23 22:03:03 by mouslimmo         #+#    #+#             */
-/*   Updated: 2022/10/02 17:42:35 by mmourdal         ###   ########.fr       */
+/*   Created: 2022/10/02 17:18:18 by mmourdal          #+#    #+#             */
+/*   Updated: 2022/10/02 17:37:03 by mmourdal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
-#ifndef LIBFT_H
-# define LIBFT_H
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
-
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-int		ft_atoi(const char *str);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-int		ft_isalnum(int c);
-char	*ft_strchr(const char *str, int c);
-size_t	ft_strlen(const char *s);
-#endif
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char) c)
+		{
+			return ((char *)&s[i]);
+		}
+		i++;
+	}
+	if (c == 0)
+	{
+		return ((char *)&s[i]);
+	}
+	return (0);
+}
--- ft_strlen.c
+++ ft_strlen.c
@@ -1,30 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmourdal <mmourdal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/23 22:03:03 by mouslimmo         #+#    #+#             */
-/*   Updated: 2022/10/02 17:42:35 by mmourdal         ###   ########.fr       */
+/*   Created: 2022/10/02 17:40:00 by mmourdal          #+#    #+#             */
+/*   Updated: 2022/10/02 17:41:25 by mmourdal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
-#ifndef LIBFT_H
-# define LIBFT_H
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
-
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-int		ft_atoi(const char *str);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-int		ft_isalnum(int c);
-char	*ft_strchr(const char *str, int c);
-size_t	ft_strlen(const char *s);
-#endif
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
