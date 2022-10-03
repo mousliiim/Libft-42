080c7ea69094cba2994459d7f62d678cc46f99b0
mousliiim <mouslimby77@gmail.com>
2022-10-03 11:09:47 +0200

    MajIsAlNum

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,16 +6,16 @@
 /*   By: mmourdal <mmourdal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 22:21:20 by mmourdal          #+#    #+#             */
-/*   Updated: 2022/09/28 22:25:49 by mmourdal         ###   ########.fr       */
+/*   Updated: 2022/10/03 11:09:15 by mmourdal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 int	ft_isalnum(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if (ft_isalpha(c))
 		return (1);
-	else if (c >= '0' && c <= '9')
+	else if (ft_isdigit(c))
 		return (1);
 	else
 		return (0);
