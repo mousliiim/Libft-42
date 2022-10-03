7766e4b97e3f50399225e51020206bb120c043b6
mousliiim <mouslimby77@gmail.com>
2022-10-03 12:48:09 +0200

    bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: mmourdal <mmourdal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 11:15:07 by mmourdal          #+#    #+#             */
-/*   Updated: 2022/10/03 11:29:05 by mmourdal         ###   ########.fr       */
+/*   Updated: 2022/10/03 12:47:47 by mmourdal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -25,20 +25,22 @@ void	ft_bzero(void *s, size_t n)
 	}
 }
 
+/*
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
     int main(void)
     {
-        char buffer[6] = "Hello!";
-	char deux[6] = "Hello!";
-	printf("Display above Bzero : %s\n", buffer);
-	bzero(buffer, 0);
-	printf("Display after Bzero : %s\n", buffer);
-	printf("________________\n");
-	printf("Display above Ft_Bzero : %s\n", deux);
-       	ft_bzero(deux, 0);
-        printf("Display after Ft_Bzero : %s\n", deux);
-        return EXIT_SUCCESS;
+		char buffer[10] = "Hello!";
+		char deux[10] = "Hello!";
+			printf("\nDisplay above Bzero : %s\n", buffer);
+			bzero(buffer, 0);
+			printf("Display after Bzero : %s\n", buffer);
+				printf("________________\n\n");
+			printf("Display above Ft_Bzero : %s\n", deux);
+			ft_bzero(deux, 0);
+			printf("Display after Ft_Bzero : %s\n\n", deux);
+        return (0);
     }
+*/
